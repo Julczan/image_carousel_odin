dcc7b68085af8a15ce4563a3f1b5bf0f1606bfef
Julczan <julek111222h@gmail.com>
2025-08-04 23:23:35 +0200

    adding classes for each slide

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("test");
+const nextArrow = document.querySelector(".next");
+const prevArrow = document.querySelector(".previous");
+
+function next() {
+  const slider = document.querySelector(".slider");
+  slider.classList.remove("slider");
+  slider.classList.add("slider-second");
+}
+
+nextArrow.addEventListener("click", () => next());
