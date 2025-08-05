c2af36bde88bc3982b9d3c39608df7e3ac7365b3
Julczan <julek111222h@gmail.com>
2025-08-05 12:23:50 +0200

    navigation buttons functionality

--- script.js
+++ script.js
@@ -1,47 +1,29 @@
 const nextArrow = document.querySelector(".next");
 const prevArrow = document.querySelector(".previous");
 const slider = document.querySelector("#slider");
+const startBtn = document.querySelector("#btn-start");
+const secondBtn = document.querySelector("#btn-second");
+const thirdBtn = document.querySelector("#btn-third");
+const lastBtn = document.querySelector("#btn-last");
 
-function next() {
-  switch (getSliderPosition()) {
-    case "slider-start":
-      slider.classList.remove("slider-start");
-      slider.classList.add("slider-second");
-      break;
-    case "slider-second":
-      slider.classList.remove("slider-second");
-      slider.classList.add("slider-third");
-      break;
-    case "slider-third":
-      slider.classList.remove("slider-third");
-      slider.classList.add("slider-last");
-      break;
-    case "slider-last":
-      slider.classList.remove("slider-last");
-      slider.classList.add("slider-start");
-      break;
-  }
-}
+function sliderPosition() {
+  const setStart = () => {
+    slider.className = "slider-start";
+  };
 
-function previous() {
-  switch (getSliderPosition()) {
-    case "slider-start":
-      slider.classList.remove("slider-start");
-      slider.classList.add("slider-last");
-      break;
-    case "slider-last":
-      slider.classList.remove("slider-last");
-      slider.classList.add("slider-third");
-      break;
-    case "slider-third":
-      slider.classList.remove("slider-third");
-      slider.classList.add("slider-second");
-      break;
-    case "slider-second":
-      slider.classList.remove("slider-second");
-      slider.classList.add("slider-start");
-      break;
-  }
+  const setSecond = () => {
+    slider.className = "slider-second";
+  };
+
+  const setThird = () => {
+    slider.className = "slider-third";
+  };
+
+  const setLast = () => {
+    slider.className = "slider-last";
+  };
+
+  return { setStart, setSecond, setThird, setLast };
 }
 
 function getSliderPosition() {
@@ -49,6 +31,47 @@ function getSliderPosition() {
 
   return sliderPosition;
 }
+
+const position = sliderPosition();
+
+function next() {
+  switch (getSliderPosition()) {
+    case "slider-start":
+      position.setSecond();
+      break;
+    case "slider-second":
+      position.setThird();
+      break;
+    case "slider-third":
+      position.setLast();
+      break;
+    case "slider-last":
+      position.setStart();
+      break;
+  }
+}
+
+function previous() {
+  switch (getSliderPosition()) {
+    case "slider-start":
+      position.setLast();
+      break;
+    case "slider-last":
+      position.setThird();
+      break;
+    case "slider-third":
+      position.setSecond();
+      break;
+    case "slider-second":
+      position.setStart();
+      break;
+  }
+}
+
+startBtn.addEventListener("click", () => position.setStart());
+secondBtn.addEventListener("click", () => position.setSecond());
+thirdBtn.addEventListener("click", () => position.setThird());
+lastBtn.addEventListener("click", () => position.setLast());
 
 nextArrow.addEventListener("click", () => next());
 prevArrow.addEventListener("click", () => previous());
